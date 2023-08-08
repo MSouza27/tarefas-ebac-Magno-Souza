f0d9c9b919062f1183e3a9debf3c0769558f0b8b
Magno Souza <127249306+MSouza27@users.noreply.github.com>
2023-08-07 22:11:53 -0300

    Add files via upload

---
+++ Tarefa_Wrappers/Resultado.java
@@ -0,0 +1,15 @@
+public class Resultado {
+    public static void main(String[] args) {
+
+            int num1 = 10;
+            System.out.println(num1);
+
+            short num2 = (short) num1;
+            System.out.println(num2);
+
+        
+    }
+
+
+    
+}
