13c531b2331e2b5819829227d0da793cba23f72a
Magno Souza <127249306+MSouza27@users.noreply.github.com>
2024-02-23 02:46:43 -0300

    Add files via upload

---
+++ PessoaFisicaJuridica/App.java
@@ -0,0 +1,27 @@
+package PessoaFisicaJuridica;
+
+public class App {
+    public static void main(String[] args) {
+        
+        PessoasFisica pessoasFisica = new PessoasFisica();
+        pessoasFisica.setNome("Magno");
+        pessoasFisica.setCpfOuCnpj(123456789L);
+        pessoasFisica.setIdade(30);
+    
+
+        PessoaJuridica pessoaJuridica = new PessoaJuridica();
+        pessoaJuridica.setNome("BBB");
+        pessoaJuridica.setCpfOuCnpj(11111111111L);
+        pessoaJuridica.setRazaoSocial("Logista");
+
+
+        System.out.println("Meu Nome: " + pessoasFisica.getNome());
+        System.out.println("Meu CPF: " + pessoasFisica.getCpfOuCnpj());
+        System.out.println("Minha Idade: " + pessoasFisica.getIdade());
+        System.out.println("********************************");
+        System.out.println("Nome da Empresa: " + pessoaJuridica.getNome());
+        System.out.println("O CNPJ é: " + pessoaJuridica.getCpfOuCnpj());
+        System.out.println("Minha razão Social é: " + pessoaJuridica.getRazaoSocial());
+
+    }
+}
---
+++ PessoaFisicaJuridica/PessoaJuridica.java
@@ -0,0 +1,27 @@
+package PessoaFisicaJuridica;
+
+public class PessoaJuridica extends DadosEmComum{
+
+    private String razaoSocial;
+
+    @Override
+    public String nome() {
+        return null;
+    }
+
+    @Override
+    public Long cpfOuCnpj() {
+        return null;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    
+    
+}
